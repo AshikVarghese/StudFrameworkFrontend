953db3b1aa9e684d5a41f759cdef89ed038eb945
Sanjay03K <75990210+Sanjay03K@users.noreply.github.com>
2022-01-19 01:35:51 +0530

    Add files via upload

--- src/views/Pages/ExtraCurricularData/ExtraCurricularData1.js
+++ src/views/Pages/ExtraCurricularData/ExtraCurricularData1.js
@@ -17,6 +17,7 @@ import {
   SimpleGrid,
   Button,
   Collapse,
+  Hide,
 } from "@chakra-ui/react";
 // Custom components
 import Card from "components/Card/Card.js";
@@ -39,11 +40,19 @@ function ExtraCurricularData() {
   const textColor = useColorModeValue("gray.700", "white");
   let params = new URLSearchParams();
   params.append("RollNumber", localStorage.getItem("generalStudent"));
+  localStorage.setItem("firsttime","yes")
   useEffect(async () => {
+      if(localStorage.getItem("proceed")=="success"){
+        axios.post(server_URL + "temp", params).then((items) => {
+          setdata(items.data)
+          localStorage.removeItem("proceed")
+        }); 
+      }  
+    });
+    
     axios.post(server_URL + "temp", params).then((items) => {
       setdata(items.data)
     });
-  });
 
   return (
     <Flex direction="column" pt={{ base: "120px", md: "65px" }}>
@@ -112,19 +121,19 @@ function ExtraCurricularData() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data[0].map((item) => {
+                  {data[0]!=undefined ? data[0].map((items) => {
                     return (
                       <ExtraCurricualarTableRow1
-                        id={item.s_no}
-                        row1={item.club_name}
-                        row2={item.activity_name}
-                        row3={item.date}
-                        row4={item.outcome}
-                        row5={item.credits}
-                        row6={item.verified}
+                        id={items.s_no}
+                        row1={items.club_name}
+                        row2={items.activity_name}
+                        row3={items.date}
+                        row4={items.outcome}
+                        row5={items.credits}
+                        row6={items.verified}
                       />
-                    );
-                  })}
+                    )
+                  }) :"HELLO"}
                 </Tbody>
               </Table>
             </CardBody>
@@ -162,7 +171,7 @@ function ExtraCurricularData() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data[1].map((item) => {
+                  {data[1]!=undefined ? data[1].map((item) => {
                     return (
                       <ExtraCurricualarTableRow2
                         id={item.s_no}
@@ -173,7 +182,7 @@ function ExtraCurricularData() {
                         row5={item.outreach_verified}
                       />
                     );
-                  })}
+                  }) : "HELLO"}
                 </Tbody>
               </Table>
             </CardBody>
@@ -212,7 +221,7 @@ function ExtraCurricularData() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data[2].map((row) => {
+                  {data[2]!=undefined ? data[2].map((row) => {
                     return (
                       <ExtraCurricualarTableRow3
                         id={row.s_no}
@@ -224,7 +233,7 @@ function ExtraCurricularData() {
                         row6={row.verified}
                       />
                     );
-                  })}
+                  }): "HELLO"}
                 </Tbody>
               </Table>
             </CardBody>
@@ -262,7 +271,7 @@ function ExtraCurricularData() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data[3].map((row) => {
+                  {data[3]!=undefined ? data[3].map((row) => {
                     return (
                       <ExtraCurricualarTableRow4
                         id={row.s_no}
@@ -273,7 +282,7 @@ function ExtraCurricularData() {
                         row5={row.verified}
                       />
                     );
-                  })}
+                  }) : "HELLO"}
                 </Tbody>
               </Table>
             </CardBody>
