1f606e02876b8f29384db054ced31231384f703f
Sanjay03K <75990210+Sanjay03K@users.noreply.github.com>
2022-01-22 16:50:15 +0530

    Add files via upload

--- src/views/Home/Profile/Profile2.js
+++ src/views/Home/Profile/Profile2.js
@@ -49,6 +49,7 @@ import { SettingsIcon, EditIcon, WarningIcon } from "@chakra-ui/icons";
 import axios from "axios";
 import ProfileBgImage from "assets/img/ProfileBackground.png";
 import { server_URL } from "controller/urls_config";
+import change_pass from "controller/changepassword";
 
 function Profile() {
   function validation() {
