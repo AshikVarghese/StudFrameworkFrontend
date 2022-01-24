efdf6ea559aabfef54e7455aa2799e6de7e8650b
priyadhanu14 <haripriyadhanasekaran14@gmail.com>
2022-01-24 18:01:17 +0530

    completed1

--- src/components/Charts/LineChartAcademics.js
+++ src/components/Charts/LineChartAcademics.js
@@ -1,121 +1,101 @@
-// import React, { Component, useState } from "react";
-// import ReactApexChart from "react-apexcharts";
-// import Card from "components/Card/Card";
-// import Chart from "react-apexcharts";
-// import { lineChartData, lineChartOptions } from "variables/charts";
+import React, { Component, useState } from "react";
+import ReactApexChart from "react-apexcharts";
+import Card from "components/Card/Card";
+import Chart from "react-apexcharts";
+//import { lineChartData, lineChartOptions } from "variables/charts";
 
-// import axios from "axios";
+import axios from "axios";
 
-// import { URL, server_URL } from "controller/urls_config";
+import { URL, server_URL } from "controller/urls_config";
 
+ const lineChartData = [
+  {
+    name: "CS123",
+    data: [50, 40],
+  },
+  {
+    name: "CS986",
+    data: [30, 90],
+  },
+  {
+    name: "CS875",
+    data: [60, 10],
+  },
+];
 
-let chartOptions = {
-    chart: {
-      toolbar: {
-        show: false,
-      },
-    },
-    tooltip: {
-      style: {
-        backgroundColor: "red",
-        fontSize: "12px",
-        fontFamily: undefined,
-      },
-      onDatasetHover: {
-        style: {
-          backgroundColor: "red",
-          fontSize: "12px",
-          fontFamily: undefined,
-        },
-      },
-      theme: "dark",
-    },
-    xaxis: {
-      categories: ["2019-2023"],
+ const lineChartOptions = {
+  chart: {
+    toolbar: {
       show: true,
-      labels: {
-        show: true,
-        style: {
-          colors: "#fff",
-          fontSize: "12px",
-        },
-      },
-      axisBorder: {
-        show: false,
-      },
-      axisTicks: {
-        show: false,
-      },
     },
-    yaxis: {
-      show: true,
-      color: "#fff",
-      labels: {
-        show: true,
-        style: {
-          colors: "#fff",
-          fontSize: "14px",
-        },
-      },
-    },
-    grid: {
-      show: false,
-    },
-    fill: {
-      colors: "#fff",
-    },
-    dataLabels: {
-      enabled: false,
-    },
-    plotOptions: {
-      bar: {
-        borderRadius: 8,
-        columnWidth: "12px",
-      },
-    },
-    responsive: [
-      {
-        breakpoint: 768,
-        options: {
-          plotOptions: {
-            bar: {
-              borderRadius: 0,
-            },
-          },
-        },
-      },
+  },
+  tooltip: {
+    theme: "dark",
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: "smooth",
+  },
+  xaxis: {
+    categories: [
+      "CAT1",
+      "CAT2",
     ],
-  };
+    labels: {
+      style: {
+        colors: "#c8cfca",
+        fontSize: "12px",
+      },
+    },
+  },
+  yaxis: {
+    labels: {
+      style: {
+        colors: "#c8cfca",
+        fontSize: "12px",
+      },
+    },
+  },
+  legend: {
+    show: false,
+  },
+  grid: {
+    strokeDashArray: 5,
+  },
+  fill: {
+    colors: ["#aba0ef", "#2D3748","#2D3790"],
+  },
+  colors: ["#aba0ef", "#2D3748","#2D3790"],
+};
+ 
+function LineChart()
+{
+    //const [intern_lst, setintern] = useState([100]);
   
-class LineChart extends React.Component {
-    constructor(props) {
-      super(props);
+    //var lineChartData = { name: "Academics", data: intern_lst };
   
-      this.state = {
-        chartData: [],
-        chartOptions: {},
-      };
-    }
+    let params = new URLSearchParams();
+    params.append("batch", localStorage.getItem("batch"));
+    params.append("dept", localStorage.getItem("dept"));
   
-    componentDidMount() {
-      this.setState({
-        chartData: lineChartData,
-        chartOptions: lineChartOptions,
+    useState(async () => {
+      axios.post(server_URL + "AcademicsGraphCA", params).then((items) => {
+         console.log(items);
+        // console.log(items.data.placement_lst);
+        // console.log(...items.data.placement_lst);
+        // setintern(items.data.intern_lst);
+        // lineChartOptions.xaxis.categories = items.data.batches;
       });
-    }
+    });
   
-    render() {
-      return (
-        <ReactApexChart
-          options={this.state.chartOptions}
-          series={this.state.chartData}
-          type="area"
-          width="100%"
-          height="100%"
-        />
-      );
-    }
-  }
+  return( <ReactApexChart
+        options={lineChartOptions}
+        series={lineChartData}
+        width="100%"
+        height="100%"
+      />);
+}
   
-  export default LineChart;
-  
+export default LineChart;
