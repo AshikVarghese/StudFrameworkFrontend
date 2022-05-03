f3b1c13b020c8ba11208b7017831cabbf0a5cb6f
sinking8 <ashwinblaze111@gmail.com>
2022-05-03 15:11:27 +0530

    added credits table for officials

--- src/views/Home/DashBoard/Dashboard3.js
+++ src/views/Home/DashBoard/Dashboard3.js
@@ -1,13 +1,21 @@
 /** @format */
 
 // Chakra imports
-import { Box, Flex, SimpleGrid, Stat, StatLabel } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  SimpleGrid,
+  Stat,
+  StatLabel,
+  Heading,
+} from "@chakra-ui/react";
 
 // Custom components
 import Card from "components/Card/Card.js";
 import CardBody from "components/Card/CardBody.js";
 import BarChartInternship from "components/Charts/BarChartInternship3";
 import BarChartPlacement from "components/Charts/BarChartPlacement3";
+import CreditsTable3 from "components/Tables/CreditsTable/creditstable3";
 
 export default function Dashboard() {
   return (
@@ -63,6 +71,12 @@ export default function Dashboard() {
           </CardBody>
         </Card>
       </SimpleGrid>
+      <Card minH="300px">
+        <Heading mb={4} color="gray.400">
+          Credits Summary
+        </Heading>
+        <CreditsTable3 />
+      </Card>
     </Flex>
   );
 }
